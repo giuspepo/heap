cf691747981c1d82d0ca8583e1be959bc8e939b0
giuspepo <gius.pepo@gmail.com>
2017-05-31 00:48:40 -0300

    Add files via upload

--- heap2.c
+++ heap2.c
@@ -13,7 +13,7 @@ struct heap{
 };
 //Esta es mi idea: El heap guarda un arreglo de datos que termina siempre en NULL.
 //y cant guarda la posicion de ese ultimo NULL.
-//De esa manera, si el heap esta vacio (cant == 0), ver_max puede aun usar heap-datos[0].
+//De esa manera, si el heap esta vacio (cant == 0), ver_max puede aun usar heap->datos[0].
 
 bool heap_redimensionar(heap_t* heap, size_t nuevo_tam){
 	void** datos = realloc( heap->datos, nuevo_tam * sizeof(void*));
@@ -31,22 +31,38 @@ void swap(void** vector, size_t a, size_t b) {
 	vector[b] = aux;
 }
 
+//Devuelve el hijo de una posicion con el valor maximo, dentro de un heap.
+//Pre: el doble se encuentra dentro del rango. [2*pos < heap->cant]
+size_t hijo_max(heap_t* heap, size_t pos) {
+	//Si los hijos estan fuera del rango, devuelve cant como error.
+	size_t izq = 2*pos+1;
+	size_t der = 2*pos+2;
+	if (izq >= heap->cant) return heap->cant;
+	if (der == heap->cant) return izq;
+	int c = heap->cmp(heap->datos[izq], heap->datos[der]);
+	if (c > 0) return izq;
+	else return der;
+}
+
 void upheap(heap_t* heap){
 	size_t pos = heap->cant - 1;
-	size_t mid = (pos - 1)/2;
+	size_t padre = (pos - 1)/2;
 	//Mid esta en caso que la division no castee correctamente.
-	while(pos > 0 &&  heap->cmp(heap->datos[mid], heap->datos[pos] ) < 0){
-		swap(heap->datos, mid, pos);
-		pos = mid;
-		mid = (pos - 1)/2;
+	while(pos > 0 &&  heap->cmp(heap->datos[padre], heap->datos[pos] ) < 0){
+		swap(heap->datos, padre, pos);
+		pos = padre;
+		padre = (pos - 1)/2;
 	}
 }
 
 void downheap(heap_t* heap){
 	size_t pos = 0;
-	while ( 2*pos < heap->cant && heap->datos[2*pos+1] != NULL && heap->cmp( heap->datos[pos], heap->datos[2*pos+1]) < 0 ){
-		swap(heap->datos, 2*pos+1, pos);
-		pos = 2*pos+1;
+	//while ( 2*pos < heap->cant && heap->datos[2*pos+1] != NULL && heap->cmp( heap->datos[pos], heap->datos[2*pos+1]) < 0 ){
+	while (2*pos + 1 < heap->cant) {
+		size_t hijo = hijo_max(heap, pos);
+		if (heap->cmp(heap->datos[pos], heap->datos[hijo]) > 0) break;
+		swap(heap->datos, pos, hijo);
+		pos = hijo;
 	}
 }
 
@@ -66,13 +82,25 @@ heap_t *heap_crear(cmp_func_t cmp){
 	return heap;
 }
 
+//Ya encontre tu error.
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
-	heap_t* heap =  malloc( sizeof (heap_t) * n);
+	heap_t* heap =  malloc( sizeof (heap_t) );
+	if (!heap) return NULL;
 
-	for (int i = 0; i < n; i++){
-		// wtf!
-		heap_encolar(heap, arreglo[i]);
+	heap->datos = malloc(sizeof(void*) * (n+1));
+	if (!heap->datos) {
+		free(heap);
+		return NULL;
 	}
+	heap->tam = n+1;
+	heap->cant = 0;
+	for (int i = 0; i < n; i++){
+		heap->datos[heap->cant] = arreglo[i];
+		heap->cant++;
+		upheap(heap);
+	}
+
+	heap->datos[n] = NULL;
 
 	return heap;
 }
@@ -121,10 +149,10 @@ void *heap_desencolar(heap_t *heap){
 	heap->datos[0] = heap->datos[heap->cant];
 	heap->datos[heap->cant] = NULL;
 
+	size_t fraccion = heap->tam /FACT_REDIM;
 	//Reduce a una fraccion el tamano del vector si se alcanzo un minimo adecuado
-	if ((heap->cant + 1)* FACT_REDIM < heap->tam) {
-		size_t frac = heap->tam /FACT_REDIM;
-		heap_redimensionar( heap, frac);
+	if (heap->cant  < fraccion) {
+		heap_redimensionar( heap, fraccion);
 	}
 
 	downheap( heap );
